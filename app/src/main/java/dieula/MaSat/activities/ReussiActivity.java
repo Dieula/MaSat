cc8347185f28fd10ccfb7f3713a2ae0198101b5c
Dieula <dareusdieula@gmail.com>
2018-02-11 12:42:03 -0800

    Adding project

--- app/src/main/java/dieula/MaSat/activities/ReussiActivity.java
+++ app/src/main/java/dieula/MaSat/activities/ReussiActivity.java
@@ -1,7 +1,9 @@
 package dieula.MaSat.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import dieula.MaSat.R;
 
@@ -12,5 +14,21 @@ public class ReussiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reussi);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                // Respond to the action bar's Up/Home button
+                Intent i = new Intent(ReussiActivity.this, MaSatActivity.class);
+                startActivity(i);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
